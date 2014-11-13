746b668739bbfbb1ae9e7be31ce3bb50c1819b80
tcandens <joey.thies@gmail.com>
2014-11-12 18:32:10 -0800

    Rough shuffle function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-$(function() {
 
-  // Adjective Swapper
-  var adjectiveList = [
-    ["Problem Solver", "a"],
-    ["Harry Potter Fan", "a"],
-    ["Graphic Designer", "a"]
-  ];
+// Adjective Swapper
+var adjectiveList = [
+  ["Problem Solver", "a"],
+  ["Harry Potter Fan", "a"],
+  ["Graphic Designer", "a"],
+  ["Asshole", "an"]
+];
 
-  var $shuffleScope = $('.shuffle-scope');
-  var $button = $shuffleScope.find('.shuffle-button');
-  var $adjective = $shuffleScope.find('.adjective');
-  var $article = $shuffleScope.find('.article');
+var $shuffleScope = $('.shuffle-scope');
+var $button = $shuffleScope.find('.shuffle-button');
+var $adjective = $shuffleScope.find('.adjective');
+var $article = $shuffleScope.find('.article');
 
-  var shuffle = function() {
-    var index = adjectiveList.length;
-    var rand = Math.floor(Math.random()) * index;
+var shuffle = function() {
+  var index = adjectiveList.length;
+  var rand = Math.floor(Math.random() * index );
+  console.log(rand);
 
-    var newAdj = adjectiveList[rand];
-  }
+  var newAdj = adjectiveList[rand][0];
+  var newArt = adjectiveList[rand][1];
 
-});
+  $adjective.text(newAdj);
+  $article.text(newArt);
+};
+
+$button.on('click', shuffle);
