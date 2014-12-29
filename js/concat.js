be42617299120395594ce95458fbb07bbc553a9f
tcandens <joey.thies@gmail.com>
2014-12-28 22:00:25 -0800

    Remove LAZY-LOAD

--- js/concat.js
+++ js/concat.js
@@ -1,15 +1,6 @@
 // Cut some mustard
 $('html').toggleClass('no-js').addClass('js-ready');
 
-// Lazy Loading By Class
-// Collect Items with class
-$lazyLoads = $('.lazy-load');
-// Show / Fade-in elements onLoad
-// $('body').addClass('lazy-load__done');
-$( window ).on('load', function(){
-  $('body').addClass('lazy-load__done');
-});
-
 // Hamburger Toggles Navigation
 $('.hamburger').on('click', function(e) {
   e.preventDefault();
